8327b00cd24ce8e76f56d004f927db07abe0b091
John Thornton <dev@gnipsel.com>
2025-08-22 11:49:45 -0500

    work on jog logic
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/keyboard_jog.py
+++ flexgui/src/libflexgui/keyboard_jog.py
@@ -1,9 +1,124 @@
+import linuxcnc as emc
 
 def jog(parent, action, axis=None, direction=None):
-	print('jog')
 	vel = parent.jog_vel_sl.value() / 60
-	if action:
-		print(f'action {action} axis {axis} direction {direction}')
-	else:
-		print(f'action {action}')
+	parent.status.poll()
+	print(f'parent.status.kinematics_type {parent.status.kinematics_type}')
+	print(f'emc.KINEMATICS_IDENTITY {emc.KINEMATICS_IDENTITY}')
+	print(f'parent.status.motion_mode {parent.status.motion_mode}')
+	print(f'emc.TRAJ_MODE_FREE {emc.TRAJ_MODE_FREE}')
+	print(f'emc.TRAJ_MODE_TELEOP {emc.TRAJ_MODE_TELEOP}')
+	#print(f'{}')
 
+	if parent.status.kinematics_type == emc.KINEMATICS_IDENTITY:
+		print('KINEMATICS_IDENTITY')
+		if parent.status.motion_mode == emc.TRAJ_MODE_FREE:
+			teleop_mode = 0
+			jog_mode = True
+		else:
+			teleop_mode = 1
+			jog_mode = False
+
+
+	else:
+		print('Unknown Kinematics')
+
+	if action:
+		print(f'jog {action} axis {axis} direction {direction} vel {vel}')
+	else:
+		print(f'jog {action}')
+
+
+'''
+
+ jog(command-constant, bool, int[, float[, float]])
+
+    Syntax
+
+        jog(command, jjogmode, joint_num_or_axis_index, velocity[, distance])
+        jog(linuxcnc.JOG_STOP, jjogmode, joint_num_or_axis_index)
+        jog(linuxcnc.JOG_CONTINUOUS, jjogmode, joint_num_or_axis_index, velocity)
+        jog(linuxcnc.JOG_INCREMENT, jjogmode, joint_num_or_axis_index, velocity, distance)
+    Command Constants
+
+        linuxcnc.JOG_STOP
+        linuxcnc.JOG_CONTINUOUS
+        linuxcnc.JOG_INCREMENT
+    jjogmode
+
+        True
+
+            request individual joint jog (requires teleop_enable(0))
+        False
+
+            request axis Cartesian coordinate jog (requires teleop_enable(1))
+
+    joint_num_or_axis_index
+
+        For joint jog (jjogmode=1)
+
+            joint_number
+        For axis Cartesian coordinate jog (jjogmode=0)
+
+            zero-based index of the axis coordinate with respect to the known coordinate letters XYZABCUVW (x=>0,y=>1,z=>2,a=>3,b=>4,c=>5,u=>6,v=>7,w=>8)
+
+
+
+		if increment:
+			parent.command.jog(emc.JOG_INCREMENT, jjogmode, joint, vel, increment)
+		else:
+			parent.command.jog(emc.JOG_CONTINUOUS, jjogmode, joint, vel)
+	else:
+		parent.command.jog(emc.JOG_STOP, jjogmode, joint)
+
+
+def get_jog_mode(parent):
+	parent.status.poll()
+	if parent.status.kinematics_type == emc.KINEMATICS_IDENTITY and utilities.all_homed(parent):
+		teleop_mode = 1
+		jjogmode = False
+	else:
+		# check motion_mode since other guis (halui) could alter it
+		if parent.status.motion_mode == emc.TRAJ_MODE_FREE:
+			teleop_mode = 0
+			jjogmode = True
+		else:
+			teleop_mode = 1
+			jjogmode = False
+
+	if ((jjogmode and parent.status.motion_mode != emc.TRAJ_MODE_FREE)
+		or (not jjogmode and parent.status.motion_mode != emc.TRAJ_MODE_TELEOP) ):
+		set_motion_teleop(parent, teleop_mode)
+	return jjogmode
+
+def set_motion_teleop(parent, value):
+	# 1:teleop, 0: joint
+	parent.command.teleop_enable(value)
+	parent.command.wait_complete()
+	parent.status.poll()
+
+def jog(parent):
+	vel = parent.jog_vel_sl.value() / 60
+	jog_command = parent.sender().objectName().split('_')
+
+	if 'minus' in jog_command:
+		vel = -vel
+
+	joint = int(jog_command[-1])
+	increment = parent.jog_modes_cb.currentData()
+
+	jjogmode = get_jog_mode(parent)
+	print(f'jjogmode {jjogmode}')
+	if parent.sender().isDown():
+		if increment:
+			parent.command.jog(emc.JOG_INCREMENT, jjogmode, joint, vel, increment)
+		else:
+			parent.command.jog(emc.JOG_CONTINUOUS, jjogmode, joint, vel)
+	else:
+		parent.command.jog(emc.JOG_STOP, jjogmode, joint)
+		if 'override_limits_cb' in parent.children:
+			parent.override_limits_cb.setChecked(False)
+			parent.override_limits_cb.setEnabled(False)
+
+
+'''
