fe619b8f77391169cc0a2fa96e8bae26cbd9aef3
John Thornton <dev@gnipsel.com>
2025-08-23 12:08:48 -0500

    keyboard jog
    Signed-off-by: John Thornton <dev@gnipsel.com>

--- flexgui/src/libflexgui/keyboard_jog.py
+++ flexgui/src/libflexgui/keyboard_jog.py
@@ -1,29 +1,34 @@
 import linuxcnc as emc
 
-def jog(parent, action, axis=None, direction=None):
+def jog(parent, action, axis, direction=None):
 	vel = parent.jog_vel_sl.value() / 60
 	if direction == 'neg':
 		vel = -vel
 	if parent.status.motion_mode == emc.TRAJ_MODE_FREE:
 		teleop_mode = 0
 		joint_jog = True
-	else:
+	else: # all axes are homed
 		teleop_mode = 1
 		joint_jog = False
 
-	print(f'jog mode text {parent.jog_modes_cb.currentText()}')
-	print(f'jog mode data {parent.jog_modes_cb.currentData()}')
+	#print(f'jog mode text {parent.jog_modes_cb.currentText()}')
+	#print(f'jog mode data {parent.jog_modes_cb.currentData()}')
 
 	if parent.status.task_mode == emc.MODE_MANUAL and action:
 		#print(f'jog {action} axis {axis} direction {direction} vel {vel}')
 
+		# set teleop mode
+		parent.command.teleop_enable(teleop_mode)
+		parent.command.wait_complete()
+
+		# set distance if jog_modes_cb.currenData() is not false
 		distance = parent.jog_modes_cb.currentData()
 		if distance:
-			print(f'parent.command.jog(.emc.JOG_INCREMENT, {joint_jog}, {axis}, {vel}, {distance})')
+			parent.command.jog(emc.JOG_INCREMENT, joint_jog, axis, vel, distance)
 		else:
-			print(f'parent.command.jog(.emc.JOG_CONTINUOUS, {joint_jog}, {axis}, {vel})')
+			parent.command.jog(emc.JOG_CONTINUOUS, joint_jog, axis, vel)
 	else:
-		print(f'parent.command.jog(.emc.JOG_STOP, {joint_jog}, {axis}')
+		parent.command.jog(emc.JOG_STOP, joint_jog, axis)
 
 	# jog_modes_cb
 
@@ -42,6 +47,10 @@ def jog(parent, action, axis=None, direction=None):
 
 
 '''
+ teleop_enable(int)
+
+    Enable/disable teleop mode (disable for joint jogging).
+
 
  jog(command-constant, bool, int[, float[, float]])
 
